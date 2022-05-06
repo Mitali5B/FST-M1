2d64c0882a8a5c08f8e14f1d982d92702becd1fe
Mitali5B <41722756+Mitali5B@users.noreply.github.com>
2022-05-06 23:12:03 +0530

    Add files via upload

---
+++ Java/Activities/JavaProject/src/main/java/JavaProject/JavaProject/BicycleOperations.java
@@ -0,0 +1,6 @@
+package JavaProject.JavaProject;
+
+public interface BicycleOperations {
+	public void applyBrake(int decrement);
+    public void speedUp(int increment);
+}
---
+++ Java/Activities/JavaProject/src/main/java/JavaProject/JavaProject/CustomException.java
@@ -0,0 +1,23 @@
+package JavaProject.JavaProject;
+
+public class CustomException extends Exception {
+    private String message = null;
+	
+ 
+	
+    public CustomException(String message) {
+	
+        this.message = message;
+	
+    }
+	
+ 
+	
+    @Override
+	
+    public String getMessage() {
+	
+        return message;
+	
+    }
+}
